fdc7b176b5ff43ac492a649f2430879febad180d
marvelman3284 <jedimaster3284@gmail.com>
2022-02-27 01:31:09 -0500

    hamming

--- src/main.py
+++ src/main.py
@@ -2,17 +2,18 @@ import numpy as np
 from functools import reduce
 import operator as op
 
+
 def hamming(data):
     data = [i for i, j in enumerate(data) if j]
 
     return reduce(op.xor, data)
+
 
 data = np.random.randint(0, 2, 16)
 
 print(data)
 
 error = hamming(data)
-
 print(error)
 
 data[error] = not data[error]
@@ -20,4 +21,3 @@ data[error] = not data[error]
 error = hamming(data)
 
 print(error)
-
